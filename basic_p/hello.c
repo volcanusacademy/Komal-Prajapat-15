69300414f1a42cbfea4d96a573246c0fe5821f8b
komal prajapt <128400276+KomalPrajapa@users.noreply.github.com>
2023-09-24 10:44:09 +0530

    Delete basic_p directory

--- basic_p/hello.c
+++
@@ -1,6 +0,0 @@
-// Write a program in C language to manipulate the structure of c programming, Print Hello Developer.
-
-#include<stdio.h>   //including header file
-void main(){        //main function
-    printf("Hello Developer");    //print our output 
-}
